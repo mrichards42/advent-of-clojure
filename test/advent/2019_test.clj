fc291db2cb4abf9b5c2090f37b1a92912f562df0
Mike Richards <mrichards@democracy.works>
2019-12-02 09:26:27 -0500

    2019 day 2

--- test/advent/2019_test.clj
+++ test/advent/2019_test.clj
@@ -2,8 +2,13 @@
   (:require [clojure.test :refer :all]
             [advent.util :as util]
             [advent-2019.day01 :as day01]
+            [advent-2019.day02 :as day02]
             ))
 
 (deftest day01-test
   (is (= 3421505 (util/run day01/part1)))
   (is (= 5129386 (util/run day01/part2))))
+
+(deftest day02-test
+  (is (= 6568671 (util/run day02/part1)))
+  (is (= 3951 (util/run day02/part2))))
