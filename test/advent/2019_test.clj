adad1f2c3b2cb39d4add672d1c376beb8fe73140
Mike Richards <mrichards@democracy.works>
2019-12-03 20:17:34 -0500

    2019 day 3

--- test/advent/2019_test.clj
+++ test/advent/2019_test.clj
@@ -3,6 +3,7 @@
             [advent.util :as util]
             [advent-2019.day01 :as day01]
             [advent-2019.day02 :as day02]
+            [advent-2019.day03 :as day03]
             ))
 
 (deftest day01-test
@@ -12,3 +13,7 @@
 (deftest day02-test
   (is (= 6568671 (util/run day02/part1)))
   (is (= 3951 (util/run day02/part2))))
+
+(deftest day03-test
+  (is (= 375 (util/run day03/part1)))
+  (is (= 14746 (util/run day03/part2))))
